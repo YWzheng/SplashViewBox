3da6cd466b04efc8505c619af8d9d639bd07bac6
zhengyongwei <zhengyongwei2015@gmail.com>
2016-04-14 10:34:19 +0800

    menu菜单添加

--- app/src/main/java/com/ywzheng/splashviewbox/MainActivity.java
+++ app/src/main/java/com/ywzheng/splashviewbox/MainActivity.java
@@ -1,8 +1,11 @@
 package com.ywzheng.splashviewbox;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 
 import butterknife.ButterKnife;
@@ -17,6 +20,26 @@ public class MainActivity extends AppCompatActivity {
         ButterKnife.inject(this);
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        Intent intent = new Intent(Intent.ACTION_VIEW);
+        int id = item.getItemId();
+        switch (id) {
+            case R.id.action_github:
+                intent.setData(Uri.parse("https://github.com/ywzheng"));
+                startActivity(intent);
+                return true;
+            default:
+                break;
+        }
+        return super.onOptionsItemSelected(item);
+    }
     @OnClick({R.id.button, R.id.button2})
     public void onClick(View view) {
         switch (view.getId()) {
